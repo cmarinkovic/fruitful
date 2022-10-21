bfa85504e6de566f15add4f153d0c350e376dec7
Cristóbal Marinkovic W <cr.marinkovic@gmail.com>
2022-10-21 10:22:43 -0300

    Changed Navbar text color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
           <Image src="/logo.png" layout="fill" objectFit="cover" alt="" />
         </div>
 
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+        <span className="self-center text-xl font-semibold whitespace-nowrap text-transparent bg-clip-text bg-gradient-to-r from-lime-700 via-lime-600 to-lime-700">
           Fruitful
         </span>
       </a>
@@ -52,10 +52,10 @@ export default function Navbar() {
     const router = useRouter();
 
     const activeLinkClass =
-      "cursor-pointer block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
+      "cursor-pointer block py-2 pr-4 pl-3 text-white bg-lime-700 rounded md:bg-transparent md:text-lime-700 md:p-0 dark:text-white";
 
     const inactiveLinkClass =
-      "cursor-pointer block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
+      "cursor-pointer block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-lime-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
 
     const getClass = (route: string) => {
       return router.pathname === route ? activeLinkClass : inactiveLinkClass;
