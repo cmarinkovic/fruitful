562e36837d5a5925812436820c902449a8d2668d
Cristóbal Marinkovic W <cr.marinkovic@gmail.com>
2022-10-24 11:09:19 -0300

    Renamed PageTitle component

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect } from "react";
+import { PropsWithChildren } from "react";
 
-interface Props {
-  title: string;
-}
-
-export default function PageTitle({ title }: Props) {
+export default function Title({ children }: PropsWithChildren) {
   return (
     <h1 className="text-2xl font-bold dark:text-white  mb-3">
       <span className="text-transparent bg-clip-text bg-gradient-to-r from-lime-700 via-lime-600 to-lime-700">
-        {title}
+        {children}
       </span>
     </h1>
   );
