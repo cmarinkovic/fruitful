2f9bc5793543b29fc109a690f8d88a5c03b0b38c
Cristóbal Marinkovic W <cr.marinkovic@gmail.com>
2022-10-25 01:29:15 -0300

    Refactores svg elements to use SVGR

--- components/AddModal.tsx
+++ components/AddModal.tsx
@@ -5,6 +5,8 @@ import { ButtonProps } from "./Button";
 
 import Title from "./Title";
 
+import CloseIcon from "../assets/close.svg";
+
 interface AddModalProps {
   title: string;
   children: React.ReactNode;
@@ -26,18 +28,7 @@ export default function AddModal({ title, children }: AddModalProps) {
         onClick={onClick}
         className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
       >
-        <svg
-          className="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
+        <CloseIcon className="w-5 h-5" />
         <span className="sr-only">Close</span>
       </button>
     );
