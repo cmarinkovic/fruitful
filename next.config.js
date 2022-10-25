0517a9e7630d7d0c7e9e9d79c43d231f5a2f101f
Cristóbal Marinkovic W <cr.marinkovic@gmail.com>
2022-10-25 01:18:07 -0300

    Added SVGR package and updated Next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   env: {
     BASE_URL: process.env.BASE_URL,
   },
